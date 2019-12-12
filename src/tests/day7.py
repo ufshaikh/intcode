f75cec791cd1627eeb36ef98a980f2a0ce0eb410
Umer Shaikh <umer.f.shaikh@gmail.com>
2019-12-12 16:09:56 -0600

    Change to a multi-threaded approach for day 7 pt 1
    For part 1 I don't think there's much advantage to this compared to
    simply sequentially looping over the machines and taking each one's
    output as input for the next. Each machine will block early in execution
    waiting for its single input, anyway. But a threaded approach is natural
    for part 2, so this should help prepare for that.

--- src/tests/day7.py
+++ src/tests/day7.py
@@ -1,26 +1,44 @@
 # script to solve day 7 problem. daisy-chains intcode machines
 
-import itertools as iter
+import itertools as it
 import subprocess as s
 import sys
+import queue
+import concurrent.futures as cs
+
+class Pipeline:
+    """Object to pass intermediate results back and forth between amplifiers."""
+    def __init__(self):
+        self.queues = list(map(queue.Queue, range(0,5)))
+        self.queues[0].put("0") # initial input
+        self.final_output = 0
+
 
 def part1():
-    phase_settings = list(iter.permutations(range(0,5)))
+    def run_machine(machine_num, phase, pipeline):
+        inp = pipeline.queues[machine_num].get()
+        arg = "stack exec intcode ~/src/intcode/src/tests/sample_inputs/input7.txt " + str(phase) + " " + inp
+        out = s.Popen(arg, shell=True, stdout=s.PIPE).stdout.read()
+        out = out.strip().decode()
+        # the final output
+        if machine_num == 4:
+            pipeline.final_output=max(int(out), pipeline.final_output)
+        # intermediate outputs are passed on
+        else:
+            pipeline.queues[machine_num+1].put(out)
 
+    num_of_machines = 5
+    phase_settings = list(it.permutations(range(0,5)))
     toret = 0
     for i in phase_settings:
-        inp = 0
-        for j in i:
-            com_str = "stack exec intcode sample_inputs/input7.txt " + str(j) + " " + str(inp)
-            inp = s.Popen(com_str, shell=True, stdout=s.PIPE).stdout.read()
-            inp = int(inp.strip().decode())
-        toret = max(toret, inp)
-
+        pipeline = Pipeline()
+        executor = cs.ThreadPoolExecutor(max_workers=5)
+        fut_objs = it.repeat(0, num_of_machines)
+        for j in range(0, num_of_machines):
+            executor.submit(run_machine, j, i[j], pipeline)
+        executor.shutdown(wait=True)
+        toret = max(toret, pipeline.final_output)
     print(toret == 17440)
-
-def part2():
-    print("testing")
-
 
 if __name__ == "__main__":
     if sys.argv[1] == "1":
@@ -28,4 +46,5 @@ if __name__ == "__main__":
     elif sys.argv[1] == "2":
         part2()
     else:
-        print("Bad argument, use '1' or '2' for part 1 or part 2.")
+        part1()
+        part2()
