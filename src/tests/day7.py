bcdaa2e8a930c3f35b3675838be818fcad4941b8
Umer Shaikh <umer.f.shaikh@gmail.com>
2019-12-14 12:49:17 -0600

    Cleanup, day 7
    Part 1 can be a special case of the part 2 strategy, so we unify the
    treatments. Adjust some style stuff.

--- src/tests/day7.py
+++ src/tests/day7.py
@@ -1,10 +1,10 @@
 # script to solve day 7 problem. daisy-chains intcode machines
 
+import concurrent.futures as cs
 import itertools as it
 import subprocess as s
 import sys
 import queue
-import concurrent.futures as cs
 
 class Pipeline:
     """Object to pass intermediate results back and forth between amplifiers."""
@@ -12,66 +12,45 @@ class Pipeline:
         self.queues = list(map(queue.Queue, range(0,5)))
         self.final_output = 0
 
-def run_machine(machine_num, phase, pipeline):
-    inp = pipeline.queues[machine_num].get()
-    arg = "stack exec intcode ~/src/intcode/src/tests/sample_inputs/input7.txt " + str(phase) + " " + str(inp)
-    out = s.Popen(arg, shell=True, stdout=s.PIPE, stderr=s.STDOUT).stdout.read()
-    out = out.strip().decode()
-    out = int(''.join(i for i in out if i.isdigit()))
-    # the final output
-    if machine_num == 4:
-        pipeline.final_output=max(out, pipeline.final_output)
-    # intermediate outputs are passed on
-    else:
-        pipeline.queues[machine_num+1].put(out)
-
-def part1():
-    num_of_machines = 5
-    phase_settings = list(it.permutations(range(0,5)))
-    toret = 0
-    for i in phase_settings:
-        pipeline = Pipeline()
-        pipeline.queues[0].put(0) # initial input
-        executor = cs.ThreadPoolExecutor(max_workers=5)
-        for j in range(0, num_of_machines):
-            executor.submit(run_machine, j, i[j], pipeline)
-        executor.shutdown(wait=True)
-        toret = max(toret, pipeline.final_output)
-    print(toret == 17440)
-
-def run_machine2(machine_num, phase, pipeline):
-    pipeline.queues[machine_num].put(phase)
-    if machine_num == 0:
-        pipeline.queues[machine_num].put(0)
-    next_mach = machine_num + 1 if machine_num < 4 else 0
+def run_amplifier(amp_num, phase, pipeline):
+    pipeline.queues[amp_num].put(phase)
+    if amp_num == 0: # initial value kicks off the whole process
+        pipeline.queues[amp_num].put(0)
+    next_amp = (amp_num + 1) % 5
     cmd = ["stack", "exec", "intcode", "sample_inputs/input7.txt"]
     p = s.Popen(cmd, stdout=s.PIPE, stdin=s.PIPE, stderr=s.STDOUT, text=True)
-    out = 'run'
+    out = 'dummy_val'
+
     while not out == '':
         out = p.stdout.readline().strip()
         out = str(out)
         if out == "Waiting for input...":
-            inp = pipeline.queues[machine_num].get()
+            inp = pipeline.queues[amp_num].get()
             inp = str(inp) + "\n"
             p.stdin.write(inp)
             p.stdin.flush()
+        # for the ending message "Done"
         elif ''.join(i for i in out if i.isdigit()) == "":
             continue
         else:
-            pipeline.queues[next_mach].put(out)
+            pipeline.queues[next_amp].put(out)
 
     p.stdin.close()
     p.terminate()
 
-def part2():
-    num_of_machines = 5
-    phase_settings = list(it.permutations(range(5,10)))
+def set_up_amplifier_pool(part):
+    num_of_amps = 5
+    if part == 1:
+        phase_settings = list(it.permutations(range(0,5)))
+    else: # part == 2:
+        phase_settings = list(it.permutations(range(5,10)))
     toret = 0
+
     for i in phase_settings:
         pipeline = Pipeline()
         executor = cs.ThreadPoolExecutor(max_workers=5)
-        for j in range(0, num_of_machines):
-            executor.submit(run_machine2, j, i[j], pipeline)
+        for j in range(0, num_of_amps):
+            executor.submit(run_amplifier, j, i[j], pipeline)
         executor.shutdown(wait=True)
         final_output = 0
         # I think we're probably guaranteed a single result in the queues that
@@ -80,18 +59,20 @@ def part2():
             if i.queue:
                 final_output = max(final_output, int(i.get()))
         toret = max(toret, final_output)
-    print(toret == 27561242)
 
-
+    if part == 1:
+        print(toret == 17440)
+    else: # part 2
+        print(toret == 27561242)
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        part1()
-        part2()
+        set_up_amplifier_pool(1)
+        set_up_amplifier_pool(2)
     elif sys.argv[1] == "1":
-        part1()
+        set_up_amplifier_pool(1)
     elif sys.argv[1] == "2":
-        part2()
+        set_up_amplifier_pool(2)
     else:
         print("Bad argument. Use '1' or '2' for part 1 or part 2, or nothing "
         "for both.")
