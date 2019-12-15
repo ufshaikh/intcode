3c5f32071d5b6cdfb9808c063bf80f98f4e3865d
Umer Shaikh <umer.f.shaikh@gmail.com>
2019-12-15 17:08:14 -0600

    Implement relative base and opc 9 (modify relbase)

--- src/Main.hs
+++ src/Main.hs
@@ -21,7 +21,7 @@ type Input = [Int]
 
 data Computer = Computer Program InsPtr RelativeBase State deriving Show
 data State = Run | Halt | GetIn | PutOut deriving (Show, Eq)
-data ParamMode = Position | Immediate deriving (Show, Eq)
+data ParamMode = Position | Immediate | Relative deriving (Eq, Ord, Show)
 data Opcode = Opcode Int deriving (Show, Ord, Eq)
 
 {- UTILITY FUNCTIONS -}
@@ -41,6 +41,7 @@ get_param_mode inst i
   | mode == Nothing = Position -- doubtless more modes incoming
   | mode == Just 0 = Position
   | mode == Just 1 = Immediate
+  | mode == Just 2 = Relative
   | otherwise = error "Bad param mode"
   where plist = drop 2 $ unfoldr get_digit inst
           where get_digit = (\x -> if x <= 0
@@ -59,6 +60,7 @@ get_num_of_params opc
   | op == 3                                      = 1
   | op == 4                                      = 1
   | op == 5 || op == 6                           = 2
+  | op == 9                                      = 1
   | op == 99                                     = 0
   where op = get_opcode_as_int opc
 
@@ -87,6 +89,10 @@ cmp (Computer prog _insptr _relbase _state) arg_list write_pos
   where result = if comp_func (head arg_list) (arg_list !! 1) then 1 else 0
         new_prog = M.insert write_pos result prog
 
+mod_relbase :: Computer -> [Int] -> Computer
+mod_relbase (Computer _prog _insptr relbase state) arg_list =
+  (Computer _prog _insptr (relbase + head arg_list) state)
+
 change_state :: Computer -> State -> Computer
 change_state (Computer _prog insptr _relbase orig_state) new_state =
   (Computer _prog insptr _relbase new_state)
@@ -103,18 +109,36 @@ upd_iptr opc (Computer _prog insptr _relbase _state) =
                        then insptr
                        else insptr + offset
 
--- NB: this gets values to use (from either mode), not to write to
-get_arg_list :: Program -> InsPtr -> Instruction -> [Int]
-get_arg_list prog insptr instruction =
-  map get_arg [0..(get_num_of_params opc - 1)]
--- findWithDefault: unlikely to matter, but just in case something
--- points beyond the map. (in that case we can be sure the val is not
--- actually used)
+get_arg :: Instruction -> InsPtr -> RelativeBase -> Program -> Int -> IWord
+get_arg instruction insptr relbase prog i
+  | mode == Immediate = M.findWithDefault 0 (insptr + i + 1) prog
+  | mode == Position = M.findWithDefault 0
+                       (M.findWithDefault 0 (insptr + i + 1) prog) prog
+  | mode == Relative = M.findWithDefault 0
+                       (relbase + (M.findWithDefault 0 (insptr + i + 1) prog)) prog
+  | otherwise = error "Bad param mode in function get_arg"
+  where mode = get_param_mode instruction i
+
+-- NB: this gets /values/ to use (from either mode), not positions
+-- (and is not to be used for writing to)
+
+get_arg_list :: Program -> InsPtr -> RelativeBase -> Instruction -> [IWord]
+get_arg_list prog insptr relbase instruction =
+  map (get_arg instruction insptr relbase prog) [0..(get_num_of_params opc - 1)]
   where opc = Opcode $ instruction `rem` 100
-        get_arg i = if (get_param_mode instruction i) == Immediate
-                      then M.findWithDefault 0 (insptr + i + 1) prog
-                      else M.findWithDefault 0
-                           (M.findWithDefault 0 (insptr + i + 1) prog) prog
+
+-- This gets the /index/ to write to, for instructions that require writing to
+-- memory
+-- This assumes that it is being called for a sensible instruction,
+-- i.e., one that writes to the position in its last parameter
+get_write_pos :: Instruction -> Opcode -> InsPtr -> RelativeBase -> Program -> Int
+get_write_pos instruction opc insptr relbase prog
+  | mode == Immediate = error "Trying to write to in immediate mode"
+  | mode == Position = prog M.! (insptr + num_of_params)
+  | mode == Relative = (+) relbase $ prog M.! (insptr + num_of_params)
+  | otherwise = error "Trying to write with a bad mode"
+    where num_of_params = get_num_of_params opc
+          mode = get_param_mode instruction $ num_of_params - 1
 
 execute :: Computer -> Instruction -> Opcode -> Computer
 execute (Computer prog insptr relbase state) instruction opc
@@ -126,13 +150,14 @@ execute (Computer prog insptr relbase state) instruction opc
   | op == 6 = jmp comp arg_list (== 0)
   | op == 7 = cmp comp arg_list write_pos (<)
   | op == 8 = cmp comp arg_list write_pos (==)
+  | op == 9 = mod_relbase comp arg_list
   | op == 99 = change_state comp Halt
   where comp = (Computer prog insptr relbase state)
         op = get_opcode_as_int opc
-        -- NB: this gets values to use (from either mode), not to write to
-        arg_list = get_arg_list prog insptr instruction
-        -- this gets the index to write to
-        write_pos = prog M.! (insptr + (get_num_of_params opc))
+        --valuse to compute with
+        arg_list = get_arg_list prog insptr relbase instruction
+        --index to write to
+        write_pos = get_write_pos instruction opc insptr relbase prog
 
 tick :: Computer -> Computer
 tick (Computer prog insptr relbase state) =
@@ -154,31 +179,41 @@ get_new_input = do
       x <- get_new_input
       return x
 
+-- TODO :: deduplicate here
 act_on_inp :: Computer -> Int -> Computer
 act_on_inp (Computer prog insptr relbase GetIn) inp =
   (Computer new_prog (insptr + (get_num_of_params (Opcode 3)) + 1) relbase Run)
-  where new_prog = M.insert (prog M.! (insptr+1)) inp prog
+  where instruction = prog M.! insptr
+        opc = Opcode $ instruction `rem` 100
+        write_pos = get_write_pos instruction opc insptr relbase prog
+        new_prog = M.insert write_pos inp prog
 
-print_out :: Program -> InsPtr -> IO ()
-print_out prog insptr =
+print_out :: Program -> InsPtr -> RelativeBase -> IO ()
+print_out prog insptr relbase =
   do
     let instruction = prog M.! insptr
-    print . head $ get_arg_list prog insptr instruction
+    print . head $ get_arg_list prog insptr relbase instruction
 
+-- use this function to debug
 run :: Input -> Computer -> IO Computer
-run input (Computer prog insptr relbase Halt) = do
+run input comp =
+  run' input comp
+  -- run' input $ traceShow comp comp
+
+run' :: Input -> Computer -> IO Computer
+run' input (Computer prog insptr relbase Halt) = do
   return (Computer prog insptr relbase Halt)
-run [] (Computer prog insptr relbase GetIn) = do
+run' [] (Computer prog insptr relbase GetIn) = do
   new_int <- get_new_input
   run (new_int:[]) (Computer prog insptr relbase GetIn)
-run (inp:inps) (Computer prog insptr relbase GetIn) = do
-  run inps $ tick $ act_on_inp (Computer prog insptr relbase GetIn) inp
+run' (inp:inps) (Computer prog insptr relbase GetIn) = do
+  run inps $ act_on_inp (Computer prog insptr relbase GetIn) inp
   where new_prog = M.insert (prog M.! (insptr+1)) inp prog
-run input (Computer prog insptr relbase PutOut) = do
-  print_out prog insptr
+run' input (Computer prog insptr relbase PutOut) = do
+  print_out prog insptr relbase
   run input $ tick (Computer prog (insptr + (get_num_of_params (Opcode 4)) + 1)
                              relbase Run)
-run input comp = run input $ tick comp
+run' input comp = run input $ tick comp
 
 main :: IO ()
 main = do
